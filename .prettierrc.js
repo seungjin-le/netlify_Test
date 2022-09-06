cdfb4eec907e5673e812357935cf70d6a89fe41c
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-06 21:31:51 +0900

    yarn zero-install setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,15 @@
 module.exports = {
-  printWidth: 80,
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  useTabs: false,
-};
+  "arrowParens": "always",
+  "bracketSpacing": true,
+  "bracketSameLine": true,
+  "embeddedLanguageFormatting": "auto",
+  "endOfLine": "lf",
+  "jsxBracketSameLine": false,
+  "printWidth": 80,
+  "quoteProps": "as-needed",
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false
+}
